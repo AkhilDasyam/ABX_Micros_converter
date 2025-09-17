aa2120db5ef9789ef5c3a5a7f3373e40c4464096
Akhil_Dasyam <126202092+AkhilDasyam@users.noreply.github.com>
2025-09-17 13:02:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, send_file, request, redirect, url_for, flash, make_response
+app = Flask(__name__) 
 
+app.secret_key = 'your_secret_key'
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
